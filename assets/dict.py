e5cb41d60ad62e03996d0bb71467b561856556bf
xx1al <114060118+xx1al@users.noreply.github.com>
2022-12-12 15:50:30 +0800

    added

---
+++ assets/dict.py
@@ -0,0 +1,8 @@
+# This is how you define a dictionary file in python
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+#This is how you print out the dict in a console
+print(houses["Harry"])
+
+#This is how you add values to the dictionary 
+houses["Hermione"]
